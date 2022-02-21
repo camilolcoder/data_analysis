e29b8361bc5d126d7216da69b7e3320984b424ba
camilolcoder <j.colmenares@uniandes.edu.co>
2022-02-20 19:01:33 -0500

    testing new data library

--- data_processing.py
+++ data_processing.py
@@ -17,6 +17,8 @@ from bs4 import BeautifulSoup
 from pytrends.request import TrendReq
 #plt.style.use('fivethirtyeight')
 
+import san
+
 import dash
 import dash_bootstrap_components as dbc
 import dash_html_components as html
@@ -24,6 +26,14 @@ import dash_core_components as dcc
 import plotly.express as px
 from dash.dependencies import Input, Output
 import plotly.graph_objects as go
+
+ohlc_df = san.get(
+"ohlc/ethereum",
+from_date="2015-01-01",
+to_date="2020-01-01",
+interval="1w"
+)
+print(ohlc_df.tail())
 
 def get_trending_topics():
     pytrend = TrendReq()
