2ae6fcd5b6079783bf7abaaaa9763694203b397b
camilolcoder <j.colmenares@uniandes.edu.co>
2022-02-23 20:14:07 -0500

    new input features

--- side_bar.py
+++ side_bar.py
@@ -153,7 +153,26 @@ def render_page_content(pathname):
                             #value='Sheet Id',
                             style={'width':'100%'},
                         )
-                    ], width=6),]),
+                    ], width=6),
+                    dbc.Col([
+                        dcc.Input(
+                            id = 'from_date',
+                            placeholder='Enter a crypto...',
+                            type='text',
+                            value='2017-01-01',
+                            style={'width':'100%'},
+                        )
+                    ], width=2),
+                    dbc.Col([
+                        dcc.Input(
+                            id = 'to_date',
+                            placeholder='Enter a crypto...',
+                            type='text',
+                            value='2022-02-02',
+                            style={'width':'100%'},
+                        )
+                    ], width=2),
+                    ], className = 'mb-2 mt-2'),
                 dbc.Row([
                     dbc.Col([
                         dbc.Card([
@@ -161,7 +180,8 @@ def render_page_content(pathname):
                                 dcc.Graph(id='crypto-trending', figure={}),
                             ])
                         ]),
-                    ], width=12),])
+                    ], width=12),
+                    ], className = 'mb-2 mt-2')
                 ]
     elif pathname == "/page-1":
         return [
@@ -247,18 +267,18 @@ def render_page_content(pathname):
 
 @app.callback(
         Output('crypto-trending', 'figure'),
-        #Output('pie-chart', 'figure'),
         Input('crypto_name', 'value'),
-        #Input('slct_year', 'value')
+        Input('from_date', 'value'),
+        Input('to_date', 'value'),
 )
 
-def update_data(crypto) #, year):
+def update_data(crypto, from_date, to_date): #, year):
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])#go.Figure()
     df1 = dp.collect_trend_score('crypto', 1)
     columns = df1.columns
     #df2 = dp.get_binance_bars('BTCUSDT', '1d', dt.datetime(2020, 1, 1), dt.datetime(2022, 2, 1))
-    df2 = dp.crypto_data(crypto)
+    df2 = dp.crypto_data(crypto, from_date, to_date)
 
     fig.add_trace(go.Scatter(x=df1.index, y=df1[columns[0]],
                     mode='lines',
@@ -280,12 +300,25 @@ def update_data(crypto) #, year):
     fig.update_yaxes(title_text="<b>BTC price</b>", secondary_y=True)
     fig.update_yaxes(title_text="<b>Trending values</b>", secondary_y=False)
 
-    # if year == 2021:
-    #     figo = go.Figure(go.Bar(x=['positive', 'negative'],y=[43 , 67]))
-    # elif year == 2022:
-    #     figo = go.Figure(go.Bar(x=['positive', 'negative'],y=[67 , 43]))
-
     return fig #, figo
+
+@app.callback(
+        #Output('crypto-trending', 'figure'),
+        Output('pie-chart', 'figure'),
+        #Input('crypto_name', 'value'),
+        Input('slct_year', 'value')
+)
+
+def update_data(year):
+
+    if year == 2021:
+        figo = go.Figure(go.Bar(x=['positive', 'negative'],y=[43 , 67]))
+    elif year == 2022:
+        figo = go.Figure(go.Bar(x=['positive', 'negative'],y=[67 , 43]))
+
+    return figo
+
+
 
 #TODO implement bootstrap for all of the pages and implement new metrics
 
