a36c5b83115f5e6c7be918679e00905a96b4d045
camilolcoder <j.colmenares@uniandes.edu.co>
2022-02-24 17:37:05 -0500

    updates for the heroku deploy

--- data_processing.py
+++ data_processing.py
@@ -1,6 +1,6 @@
 from praw import reddit
 #import config #KEYS
-import tweet_key as tk #Twitter KEYS
+#import tweet_key as tk #Twitter KEYS
 import praw
 import time
 import requests
@@ -12,8 +12,8 @@ import tweepy
 import textblob
 import re
 import pandas as pd
-import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
+#import matplotlib.pyplot as plt
+#from bs4 import BeautifulSoup
 from pytrends.request import TrendReq
 #plt.style.use('fivethirtyeight')
 from datetime import date
@@ -127,21 +127,21 @@ def get_binance_bars(symbol, interval, startTime, endTime):
 
 # print(df)
 
-def plot_trendin_sentiment(df1, df2):
+# def plot_trendin_sentiment(df1, df2):
 
-    plt.title('Trending sentiment vs Ethereum price')
-    plt.xlabel('Date')
-    plt.ylabel('Values')
+#     plt.title('Trending sentiment vs Ethereum price')
+#     plt.xlabel('Date')
+#     plt.ylabel('Values')
 
-    columns = df1.columns
-    print(columns)
+#     columns = df1.columns
+#     print(columns)
 
-    plt.plot(df1.index, df1[columns[0]], color='darkslategray', linewidth=2)
-    plt.plot(df2.index, df2.close, color='orange', linewidth=2)
+#     plt.plot(df1.index, df1[columns[0]], color='darkslategray', linewidth=2)
+#     plt.plot(df2.index, df2.close, color='orange', linewidth=2)
 
-    plt.legend(['Trending', 'ETH price'], loc='upper left')
+#     plt.legend(['Trending', 'ETH price'], loc='upper left')
 
-    plt.savefig('telegram_crypto_bot/test.png')
+#     plt.savefig('telegram_crypto_bot/test.png')
     #plt.show()
 
 #print(collect_trend_score('cryptocurrency'))
@@ -150,93 +150,85 @@ def plot_trendin_sentiment(df1, df2):
 
 #plot_trendin_sentiment(dff, df)
 
-def initialize_reddit():
-    reddit = praw.Reddit(client_id = config.client_id,
-    client_secret = config.client_secret, user_agent = config.user_agent)
-    return reddit
+# def initialize_reddit():
+#     reddit = praw.Reddit(client_id = config.client_id,
+#     client_secret = config.client_secret, user_agent = config.user_agent)
+#     return reddit
 
-def show_data(input_text, coin):
+# def show_data(input_text, coin):
 
-    if input_text == 'get coin':
-        return get_data(coin)
+#     if input_text == 'get coin':
+#         return get_data(coin)
 
-def get_data(coin):
-    try:
-        web_page = requests.get('https://www.coingecko.com/en/coins/'+coin)
-    except:
-        return 'error, couldnt find that coin'
-    web_page = web_page.content
-    soup = BeautifulSoup(web_page, 'html.parser')
-    for child in soup.body.children:
-        try:
-            for i in child.children:
-                try:
-                    price = i.div.span.text
-                    if '$' in price:
-                        return price
-                except:
-                    None
-        except:
-            None
+# def get_data(coin):
+#     try:
+#         web_page = requests.get('https://www.coingecko.com/en/coins/'+coin)
+#     except:
+#         return 'error, couldnt find that coin'
+#     web_page = web_page.content
+#     soup = BeautifulSoup(web_page, 'html.parser')
+#     for child in soup.body.children:
+#         try:
+#             for i in child.children:
+#                 try:
+#                     price = i.div.span.text
+#                     if '$' in price:
+#                         return price
+#                 except:
+#                     None
+#         except:
+#             None
 
 def format_price(price):
     price = price.split('$')
     price = price[1].replace(',','')
     return price
 
-def get_crypto_comments(subreddit):
-    reddit = initialize_reddit()
-    hot_post = reddit.subreddit(subreddit).new(limit = 200)
-    titles = []
-    for post in hot_post:
-        if post.score >= 500:
-            #print('#-----------------------------------------#------------------------------------#')
-            title = post.title.lower()
-            titles.append(title)
-    return titles
+# def get_crypto_comments(subreddit):
+#     reddit = initialize_reddit()
+#     hot_post = reddit.subreddit(subreddit).new(limit = 200)
+#     titles = []
+#     for post in hot_post:
+#         if post.score >= 500:
+#             #print('#-----------------------------------------#------------------------------------#')
+#             title = post.title.lower()
+#             titles.append(title)
+#     return titles
 
-def initialize_twitter():
+# def initialize_twitter():
     
-    authenticator = tweepy.OAuthHandler(tk.API_KEY, tk.API_KEY_SECRET)
-    authenticator.set_access_token(tk.ACCESS_TOKEN, tk.ACCESSS_TOKEN_SECRET)
+#     authenticator = tweepy.OAuthHandler(tk.API_KEY, tk.API_KEY_SECRET)
+#     authenticator.set_access_token(tk.ACCESS_TOKEN, tk.ACCESSS_TOKEN_SECRET)
 
-    api = tweepy.API(authenticator, wait_on_rate_limit=True)
+#     api = tweepy.API(authenticator, wait_on_rate_limit=True)
     
-    return api
+#     return api
 
-def search_sentiment(coin:str):
+# def search_sentiment(coin:str):
 
-    api = initialize_twitter()
+#     api = initialize_twitter()
 
-    search = f'#{coin} -filter:retweets'
+#     search = f'#{coin} -filter:retweets'
 
-    tweet_cursor = tweepy.Cursor(api.search_tweets, q=search, lang='en', 
-    tweet_mode='extended').items(100)
+#     tweet_cursor = tweepy.Cursor(api.search_tweets, q=search, lang='en', 
+#     tweet_mode='extended').items(100)
 
-    tweets = [tweet.full_text for tweet in tweet_cursor]
+#     tweets = [tweet.full_text for tweet in tweet_cursor]
 
-    tweets_df = pd.DataFrame(tweets, columns=['Tweets'])
+#     tweets_df = pd.DataFrame(tweets, columns=['Tweets'])
 
-    for _, row in tweets_df.iterrows():
-        row['Tweets'] = re.sub('http\S+', '', row['Tweets'])
-        row['Tweets'] = re.sub('#\S+', '', row['Tweets'])
-        row['Tweets'] = re.sub('@\S+', '', row['Tweets'])
-        row['Tweets'] = re.sub('\\n', '', row['Tweets'])
+#     for _, row in tweets_df.iterrows():
+#         row['Tweets'] = re.sub('http\S+', '', row['Tweets'])
+#         row['Tweets'] = re.sub('#\S+', '', row['Tweets'])
+#         row['Tweets'] = re.sub('@\S+', '', row['Tweets'])
+#         row['Tweets'] = re.sub('\\n', '', row['Tweets'])
     
-    tweets_df['Polarity'] = tweets_df['Tweets'].map(lambda tweet: textblob.TextBlob(tweet).sentiment.polarity)
+#     tweets_df['Polarity'] = tweets_df['Tweets'].map(lambda tweet: textblob.TextBlob(tweet).sentiment.polarity)
 
-    tweets_df['Result'] = tweets_df['Polarity'].map(lambda pol: '+' if pol > 0 else '-')
+#     tweets_df['Result'] = tweets_df['Polarity'].map(lambda pol: '+' if pol > 0 else '-')
 
-    positive = tweets_df[tweets_df.Result == '+'].count()['Tweets']
+#     positive = tweets_df[tweets_df.Result == '+'].count()['Tweets']
 
-    negative = tweets_df[tweets_df.Result == '-'].count()['Tweets']
+#     negative = tweets_df[tweets_df.Result == '-'].count()['Tweets']
 
-    return positive, negative
-    
-    # plt.title(coin)
-    # plt.bar(['positive', 'negative'], [positive, negative],
-    # color=['limegreen', 'red'], edgecolor='black', linewidth=1)
-    # plt.xlabel('Sentiment')
-    # plt.ylabel('Points')
-    # plt.legend()
-    # plt.show()
+#     return positive, negative
