16f20bf9578c78837521713b8c282bbbe7719353
camilolcoder <j.colmenares@uniandes.edu.co>
2022-03-02 00:00:44 -0500

    error with the san library

--- data_processing.py
+++ data_processing.py
@@ -59,10 +59,15 @@ def crypto_positive_sentiment(crypto:str, from_date:str, to_date:str):
     )
     return net_growth_df
 
-
+# san.get(
+#     "daily_active_addresses/santiment",
+#     from_date="2018-06-01",
+#     to_date="2018-06-05",
+#     interval="1d"
+# )
 
 #print(crypto_positive_sentiment('ethereum', '2017-01-01', '2020-02-20'))
-print(crypto_growth('ethereum', '2022-01-01', '2022-02-10'))
+#print(crypto_data('ethereum', '2017-01-01', '2022-02-10'))
 
 #print(crypto_holders())
 
