a4db64db6509edf41d8b9545d729235da9f27935
camilolcoder <j.colmenares@uniandes.edu.co>
2022-03-29 10:23:46 -0500

    new testing graph

--- main_app.py
+++ main_app.py
@@ -694,7 +694,35 @@ def render_page_content(pathname):
         #fig1.update_yaxes(title_text="<b>Price BTC</b>", type='log', range=[1.85,5]) #, type='linear'
         fig3.update_yaxes(title_text="<b>S&P500 price</b>", secondary_y=True)#, type='log')
         fig3.update_yaxes(title_text="<b>Recessions</b>", secondary_y=False)
+        
+        fig4 = go.Figure()
 
+# Add scatter trace for line
+        fig4.add_trace(go.Scatter(
+            x=["2015-02-01", "2015-02-02", "2015-02-03", "2015-02-04", "2015-02-05",
+            "2015-02-06", "2015-02-07", "2015-02-08", "2015-02-09", "2015-02-10",
+            "2015-02-11", "2015-02-12", "2015-02-13", "2015-02-14", "2015-02-15",
+            "2015-02-16", "2015-02-17", "2015-02-18", "2015-02-19", "2015-02-20",
+            "2015-02-21", "2015-02-22", "2015-02-23", "2015-02-24", "2015-02-25",
+            "2015-02-26", "2015-02-27", "2015-02-28"],
+            y=[-14, -17, -8, -4, -7, -10, -12, -14, -12, -7, -11, -7, -18, -14, -14,
+            -16, -13, -7, -8, -14, -8, -3, -9, -9, -4, -13, -9, -6],
+            mode="lines",
+            name="temperature"
+        ))
+
+        # Add shape regions
+        fig4.add_vrect(
+            x0="2015-02-04", x1="2015-02-06",
+            fillcolor="LightSalmon", opacity=0.5,
+            layer="below", line_width=0,
+        ),
+
+        fig4.add_vrect(
+            x0="2015-02-20", x1="2015-02-22",
+            fillcolor="LightSalmon", opacity=0.5,
+            layer="below", line_width=0,
+        )
 
         return [
                 dbc.Row([
@@ -739,6 +767,15 @@ def render_page_content(pathname):
                         dbc.Card([
                             dbc.CardBody([
                                 dcc.Graph(id='s&p500-res', figure=fig3),
+                            ])
+                        ]),
+                    ], width=12),], className = 'mb-2 mt-2'),
+                
+                dbc.Row([
+                    dbc.Col([
+                        dbc.Card([
+                            dbc.CardBody([
+                                dcc.Graph(id='s&p500-test', figure=fig4),
                             ])
                         ]),
                     ], width=12),], className = 'mb-2 mt-2')
